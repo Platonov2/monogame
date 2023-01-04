07bb86eca154dc80f2eefa787b2b8098f01b34fc
ATaoIi <107428501+ATaoIi@users.noreply.github.com>
2023-01-04 23:37:50 +0300

    Add files via upload

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from game import models
+admin.site.register(models.UsersInfo)
+admin.site.register(models.operatingAccuracy)
+admin.site.register(models.state)
+admin.site.register(models.result)
--- game/models.py
+++ game/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class UsersInfo(models.Model):
+    name = models.CharField(max_length=32)
+    surname=models.CharField(max_length=32)
+
+class  operatingAccuracy(models.Model):
+       user=models.ForeignKey(UsersInfo,on_delete=models.CASCADE)
+       #Положительное или отрицательное значение после каждого клика
+       left_accuracy=models.FloatField()
+       middle_accuracy=models.FloatField()
+       right_accuracy=models.FloatField()
+
+class state(models.Model):
+      user=models.ForeignKey(UsersInfo,on_delete=models.CASCADE)
+      previous_stressIndex = models.FloatField()
+      after_stressIndex = models.FloatField()
+      strart_lf_hf = models.FloatField()
+      end_lf_hf = models.FloatField()
+
+
+class result(models.Model):
+    user = models.ForeignKey(UsersInfo, on_delete=models.CASCADE)
+    monotony=models.BooleanField()
+
+
